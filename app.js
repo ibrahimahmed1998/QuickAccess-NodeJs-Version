d8e1ac4d1d700c3f2730272e72bab78a2e235704
hema <hema.1998.man@gmail.com>
2022-04-25 15:09:53 +0200

    w

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000; // || 3000
+const port = process.env.PORT || 5000;  
 app.use(express.static(__dirname + '/public'));
 app.use('/images', express.static('images'));
 app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.urlencoded({ extended: true }));
 const router = require('./routes/router');
 app.use('/', router);
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
