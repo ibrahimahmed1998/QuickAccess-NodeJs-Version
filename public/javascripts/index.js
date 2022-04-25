bbf94985f7b1645ba744028b5ec5f17a6d9ad195
hema <hema.1998.man@gmail.com>
2022-04-25 16:55:47 +0200

    sad

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,11 @@
-function renew()
-{
+function renew(){
     var delete_center1 = document.getElementById("center1");
     var delete_center2 = document.getElementById("center2");
 
     delete_center1.remove();
     delete_center2.remove();
     console.log("delete center");
-
-}
+ }
 
 function get(arr,choice,counter){      // body>>center>>a[img-title]>>br      wait to add updown html
 
@@ -25,13 +23,10 @@ function get(arr,choice,counter){      // body>>center>>a[img-title]>>br      wa
     center2.setAttribute("class", "down");
     center2.setAttribute("id", "center2");
 
-    for (var i = 0; i < arr.length ; i++ ) {
-
-          
+    for (var i = 0; i < arr.length ; i++ ) 
+    {
         var link = document.createElement("a");
         link.setAttribute("href", arr[i].link);
-        // link.setAttribute("id", arr[i].id);
-
         var img = document.createElement("img");
         img.setAttribute("src", "images/" + arr[i].img_path);
         img.setAttribute("title", arr[i].title);
@@ -40,12 +35,12 @@ function get(arr,choice,counter){      // body>>center>>a[img-title]>>br      wa
         if(arr[i].updown == 1 && choice == arr[i].html) { center1.appendChild(link); }
         else if (arr[i].updown == 0 && choice == arr[i].html) {  center2.appendChild(link);  }
   
-                if( i%9 == 0 && i!=0    ) 
-                { 
-                        var br = document.createElement("br");
-                        center1.appendChild(br);
-                        // center2.appendChild(br);
-                }
+        if( arr[i].counter%9 == 0 ) 
+        { 
+                var br = document.createElement("br");
+                center1.appendChild(br);
+                // center2.appendChild(br);
+        }
     }
     body.appendChild(center1);
     body.appendChild(center2);
